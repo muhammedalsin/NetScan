aede91de7ed514b375049f449f9e12f88e086d59
Muhammed  Alsin <81310513+muhammedalsin@users.noreply.github.com>
2024-06-25 16:05:37 +0300

    Update NetScan.sh

--- NetScan.sh
+++ NetScan.sh
@@ -1,17 +1,19 @@
-if [ $1 == "--help" ]
-then echo "This tool for discover hosts in network, if you want to run this tool please type ./NetScan --run"
-fi
-if [ $1 == "--run"  ]
-then ip=$(ifconfig eth0 | grep netmask | cut -d "n" -f 2 | sed 's/et //' | cut -d "." -f 1,2,3,4)
-octet=1
-echo "NetScan is running ..."
-while [ $octet -lt 255 ]
-do ipv4=$(ping -c 1 $ip.$octet | grep ttl | cut -d ":" -f 1 | sed 's/64  //')
-if [ ! -z "$ipv4"]
+#!/bin/bash
 
-then echo "               Host Found , IP : $ipv4"
-fi
-octet=$(($octet + 1))
-done
+if [ "$1" == "--help" ]; then
+    echo "This tool for discovering hosts in the network. If you want to run this tool, please type ./NetScan --run"
+    exit 0
 fi
 
+if [ "$1" == "--run" ]; then
+    ip=$(ifconfig eth0 | grep 'inet ' | awk '{print $2}' | cut -d "." -f 1,2,3)
+    octet=1
+    echo "NetScan is running ..."
+    while [ $octet -lt 255 ]; do
+        ipv4=$(ping -c 1 $ip.$octet | grep ttl | awk '{print $4}' | sed 's/://')
+        if [ ! -z "$ipv4" ]; then
+            echo "               Host Found, IP: $ipv4"
+        fi
+        octet=$(($octet + 1))
+    done
+fi
